5db7575ba849bb5f9bc81febf171b0c457c0c672
Fausto Brito <G30@Faustos-MacBook-Air.local>
2016-10-12 19:43:03 -0400

    tough one

--- liri.js
+++ liri.js
@@ -1,15 +1,21 @@
+//modules!
 var Twitter = require('twitter');
 //Grabs the export keys from keys.js
-var twitter = require('./keys.js');
-/*console.log(twitter);
+var key = require('./keys.js');
+/*console.log(key);
 */
-var twittKey = twitter.twitterKeys;
-/*console.log(twittKey);
-*/
-var client = process.argv[2];
+var twittKey = key.twitterKeys;
+
+/*console.log(twittKey);*/
+
+var client = new Twitter(twittKey);
+
+
+/*var client = process.argv[2];*/
+
 //
-var params = {screen_name: 'nodejs'};
-twittKey.get('statuses/user_timeline', params, function(error, tweets, response) {
+var params = {screen_name: 'client'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
   }
