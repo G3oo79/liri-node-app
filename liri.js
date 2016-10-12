e6ef03b4ea90ab929efbb3cdf93ece4bad828ad7
Fausto Brito <G30@Faustos-MacBook-Air.local>
2016-10-12 19:47:04 -0400

    tough one2

--- liri.js
+++ liri.js
@@ -1,19 +1,22 @@
-//modules!
+//----NPM modules! required! to be called---
 var Twitter = require('twitter');
-//Grabs the export keys from keys.js
+var spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+//------Console variables----
+var userArg = process.argv[2];
+var userArgv = process.argv[3];
+
+//-----Grabs the export from keys.js----
 var key = require('./keys.js');
-/*console.log(key);
-*/
+//----Assigns keys to variable----
 var twittKey = key.twitterKeys;
-
-/*console.log(twittKey);*/
-
+//----------
 var client = new Twitter(twittKey);
 
 
-/*var client = process.argv[2];*/
-
-//
+//User -----inputs confirm!-------
+if 
 var params = {screen_name: 'client'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
