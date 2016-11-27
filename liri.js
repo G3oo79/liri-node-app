36b1865adad977649b203925c9f4bbacf9540030
Fausto Brito <G30@Faustos-MacBook-Air.local>
2016-11-27 15:07:17 -0500

    hello

--- liri.js
+++ liri.js
@@ -9,129 +9,124 @@ var spotify = require('spotify');
 var request = require('request');
 
 var fs = require('fs');
-//+++++++++++++++++++++++++++++
-var userArg = process.argv[2];
 
-var userArgv = process.argv[3];
+//+++++++++++++Tweets Function+++++++++++++++
+//////////////////////////////////////////////
+/////////////////////////////////////////////
 
-var userInput =[]; 
+var getTweets = function(){
 
-////////////Capture User/////////////////////////
-/////////////////////////////////////////////////////
-for (var i = 2; i < process.argv.length; i++) {
-    var array = []
-    userInput.push(process.argv[i]);
-    /*userInput = array.toString().replace(',',' ');*/
-    
-    /*console.log(userInput);*/
+    var client = new Twitter(key.twitterKeys);
+
+       var params = {screen_name: 'G30o0'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+       /* console.log(tweets);*/
+       for (var i = 0; i < tweets.length; i++) {
+          console.log(tweets[i].created_at);
+          console.log(' ');
+          console.log(tweets[i].text);
+       }
+      }
+    });
+}/////////////////////////End Tweets////////////////////////
+
+//+++++++++++++++++++++++++Spotify Function+++++++++++++++++
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+var getArtistNames = function(artist) {
+    return artist.name;
 }
 
-//-----Grabs the export from keys.js----
-var key = require('./keys.js');
-//----Assigns keys to variable----
-var twittKey = key.twitterKeys;
+var getSpotify = function(songName){
 
-var client = new Twitter(twittKey);
-
-
-// -------------twitter Func-----------------
-
-function getTweet() {
-
-var params = {screen_name: 'G30o0'};
-
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-
-  if (!error) {
-
-    for (var i = 0; i < 20; i++) {
-
-    	if (tweets[i] == null || tweets[i] == undefined) {
-    		break;
-    	}
-
-    	console.log(tweets[i].text);
-
-    }
-
-    
-  }
-});
-
-}
-//-----------end twitter function-----------
-
-//------------spotify function--------------
-function getSpotify() {
-
-	spotify.search({ type:'track', query:userArgv }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-    //artist name
-    for (var x = 0; x < 10; x++){
-		console.log("Artist: " + data.tracks.items[x].artists[0].name);
-		console.log("Album: " + data.tracks.items[x].album.name);
-		console.log("Song title: " + data.tracks.items[x].name);
-	   	console.log("Preview: " + data.tracks.items[x].preview_url);
-        break;
-			    } 
-
- 
-    /*console.log(JSON.stringify(data, null, 2));*/
-	
-
-});
-}
-///////********************End Spotify************************
-////////+++++++++++++++++++++++++++++++Omdb functions-------------------------///
-
-function getOmdb(userInput) {
-
-    /*console.log(userInput)*/
-
-    userInput = userInput.toString().replace(',','');
-
-    /*console.log(userInput);*/
-
-    var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&r=json"
-
-   /* console.log(queryUrl);*/
-
-	request(queryUrl, function(error, response, body){
-
-        if (!error && response.statusCode == 200) {
-            
-
-            console.log('Title: ',JSON.parse(body)['Title']);
-            console.log('Year: ',JSON.parse(body)['Released']);
-            console.log('Rating: ',JSON.parse(body)['imbdRating']);
-            console.log('Country: ',JSON.parse(body)['Country']);
-            console.log('Language: ',JSON.parse(body)['Language']);
-            console.log('Plot: ',JSON.parse(body)['Plot']);
-            console.log('Actors: ',JSON.parse(body)['Actors']);
-            console.log('Tomatoes Rating: ',JSON.parse(body)['tomatoMeter']);
-            console.log('Tomato URL: ',JSON.parse(body)['tomatoURL']);
+    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
         }
-            /*console.log(JSON.stringify(response, null, 2));*/
+     
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+            console.log[i];
+            console.log('Artist: ' + songs[i].artists.map(getArtistNames));
+            console.log('Song Name: ' + songs[i].name);
+            console.log('Preview Song: ' + songs[i].preview_url);
+            console.log('Album: ' + songs[i].album.name);
+            console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++')
+        }
+    });
+
+}////////////////////////////End Spotify//////////////////////////
+
+//+++++++++++++++++++++++++++++OMDB Function+++++++++++++++++++++++++++++
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+var getMovie = function(movieName) {
+
+    request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        
+        var jsonData = JSON.parse(body);
+
+        console.log('Title: ' + jsonData.Title);
+        console.log('Year: ' + jsonData.Year);
+        console.log('Rated: ' + jsonData.Rated);
+        console.log('IMDB Rating: ' + jsonData.imdbRating);
+        console.log('Country: ' + jsonData.Country);
+        console.log('Language: ' + jsonData.Language);
+        console.log('Plot: ' + jsonData.Plot);
+        console.log('Actors: ' + jsonData.Actors);
+        /*console.log('Rotten Tomatoes: ' + jsonData.tomatoRating);
+        console.log('Tomatoes URL: ' + jsonData.tomatoURL);*/
+      }
+    });
+
+}///////////////////////////End OMDB///////////////////////////////
+
+//++++++++++++++++++++++++++++++++Get User Inputs+++++++++++++++++++++++
+///////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+var doWhatItSays = function(){
+
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+      if (err) throw err;
+      
+      var dataArr = data.split(',');
+
+      if (dataArr.lenght == 2) {
+        pick(dataArr[0], dataArr[1])
+      }else if (dataArr.lenght ==1) {
+        pick(dataArr[0]);
+      }
 
     });
 }
 
-/////////////////////////Function to activate user search/////////////////
-//************************************************************************
+var pick = function(caseData, functionData){
+    switch(caseData){
+        case 'my-tweets' :
+            getTweets();
+            break;
+        case 'spotify-this-song' :
+            getSpotify(functionData);
+            break;
+        case 'movie-this' :
+            getMovie(functionData);
+        case 'do it' :
+            doWhatItSays();
+            break;
+    default:
+    console.log('Check your self Before you wreck your self!!')
+    }
+}
+ var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+ };
 
-
-getSpotify();
-getTweet();
-getOmdb(userInput);
-
-
-
-
-
-
+ runThis(process.argv[2], process.argv[3]);
 
 
 
